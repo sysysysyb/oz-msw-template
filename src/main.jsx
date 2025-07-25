7624d49e54849b03c610fbcff74bebfd8a1694ab
sysysysyb <87892147+sysysysyb@users.noreply.github.com>
2025-07-25 17:20:53 +0900

    ✨ Feat: MSW 실행을 위해 필요한 파일 추가

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,24 @@ import "./index.css";
 import App from "./App.jsx";
 import { UserProvider } from "./contexts/UserContext.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
-  </StrictMode>
-);
+const enableMocking = async () => {
+  // 개발 환경에서만 실행하기 위해 개발 환경이 아니면 함수 반환
+  if (!import.meta.env.DEV) {
+    return;
+  }
+
+  // MSW를 실행하기 위한 import
+  const { worker } = await import("./mock/browser.js");
+  return worker.start();
+};
+
+// MSW 연결 후 React 렌더링
+enableMocking().then(() => {
+  createRoot(document.getElementById("root")).render(
+    <StrictMode>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </StrictMode>
+  );
+});
